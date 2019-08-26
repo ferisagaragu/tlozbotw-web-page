a81891e28854b29abc42b1dfe8b89a8e9658f38c
Enid <medina.enid@gmail.com>
2019-08-26 16:30:25 -0500

    Experimental and font

---
+++ src/modules/experimental/experimental.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Experimental extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Experimento xD</h2>
+            </div>
+        );
+    }
+}
+
+export default Experimental;
